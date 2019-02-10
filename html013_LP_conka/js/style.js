c558c01243214fed23cb36106a6e5671356f520f
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-02-10 22:49:22 +0900

    変更 トップへ移動するアイコンの修正

--- html013_LP_conka/js/style.js
+++ html013_LP_conka/js/style.js
@@ -5,12 +5,8 @@ $(function(){
 		var deru = 100
 
 		if(y < deru){
-			console.log("100より小さい");
-			console.log(y);
 			$('#top-go').fadeOut();
 		}else{
-			console.log("100より大きい");
-			console.log(y-10);
 			$('#top-go').fadeIn();
 		}
 		
