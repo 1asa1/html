7ac3afe10027b090f5be6daeb161eeecd70126db
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-02-11 01:07:51 +0900

    変更 jquryで動きをつける

--- html013_LP_conka/js/style.js
+++ html013_LP_conka/js/style.js
@@ -1,15 +1,28 @@
+//ふわっと現れるボタン（トップには戻らない）
 $(function(){
 	
-	$(window).scroll(function(){
-		var y = $(window).scrollTop();
-		var deru = 100
 
-		if(y < deru){
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		if(scroll < 100){
 			$('#top-go').fadeOut();
 		}else{
 			$('#top-go').fadeIn();
 		}
 		
-	})
+		$(".hukidashi").each(function() {
+		// 現在位置の取得とactiveをつける
+		var position = $(this).offset().top;
+		var windowHeight = $(window).height();
 
-})
+		if(scroll > position - windowHeight +200){
+			$(this).addClass('active');
+		}
+		if(scroll < 100){
+			$(this).removeClass('active');
+		}
+	});
+
+	});
+});
+
