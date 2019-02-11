d6841ddbcaa69986fae16c398b8f14593edcd944
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-02-11 12:58:49 +0900

    変更 トップへ戻るボタンの修正

--- html013_LP_conca/js/style.js
+++ html013_LP_conca/js/style.js
@@ -1,9 +1,13 @@
-//ふわっと現れるボタン（トップには戻らない）
+
 $(function(){
 	
 
-	$(window).scroll(function(){
-		var scroll = $(window).scrollTop();
+	$(window).scroll(function(){  //スクロールするたびにこれを繰り返す
+		var scroll = $(window).scrollTop(); //現在のスクロール位置を取得
+		var windowHeight = $(window).height(); //ウインドウの高さを取得
+
+		//ふわっと現れるボタン（トップには戻らない）
+		$('#top-go').hide;
 		if(scroll < 100){
 			$('#top-go').fadeOut();
 		}else{
@@ -12,16 +16,26 @@ $(function(){
 		
 		$(".hukidashi").each(function() {
 		// 現在位置の取得とactiveをつける
-		var position = $(this).offset().top;
-		var windowHeight = $(window).height();
+			var position = $(this).offset().top; // 横からスライドインするやつの
+			if(scroll > position - windowHeight + 100){
+					$(this).addClass('active');
+				}
+			if(scroll < 100){
+				$(this).removeClass('active');
+			}
+		});
 
-		if(scroll > position - windowHeight +200){
-			$(this).addClass('active');
-		}
-		if(scroll < 100){
-			$(this).removeClass('active');
-		}
-	});
+		$('.fadein').each(function(){
+			//現在位置の取得とactiveをつける
+			var fadepos = $(this).offset().top; //下からフェードインするやつの
+			if(scroll > fadepos - windowHeight + 50){
+				$(this).addClass('fadeactive');
+			}
+			if(scroll < 100){
+				$(this).removeClass('fadeactive');
+			}
+
+		});
 
 	});
 });
