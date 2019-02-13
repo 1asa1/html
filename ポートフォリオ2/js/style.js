af831a8e4f38b506f6c37b33524b901af2abe39c
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-02-14 00:00:36 +0900

    変更 ハンバーガーメニュー用jquery

--- ポートフォリオ2/js/style.js
+++ ポートフォリオ2/js/style.js
@@ -1,3 +1,5 @@
+
+//タブメニュー用
 $(function(){
 	$('#tabmenu li').on('click',function(){
 		var index = $('#tabmenu li').index(this);
@@ -6,6 +8,36 @@ $(function(){
 		$(this).addClass('active');
 		$('#tabbox section').removeClass('show');
 		$('#tabbox section').eq(index).addClass('show');
-		console.log(index);
 	});
+
+
+	//スマホ時のハンバーガーメニュー用の挙動
+	$('#toggle').on('click',function(){
+		if($(this).hasClass('toggle-active')){
+			$(this).removeClass('toggle-active');
+			$('#toggle-menu').fadeOut();
+		}else{
+			$(this).addClass('toggle-active');
+			$('#toggle-menu').fadeIn();
+		}
+	})
+
+
+	//ウインドウがリサイズされるたびに行う挙動
+	$(window).resize(function(){
+		//スマホ用ハンバーガーメニューでクリックしたときにメニュー自体を消す
+		if($(window).width() < 767){
+			$('#tabmenu li').on('click', function(){
+				$('#toggle-menu').fadeOut();
+				$('#toggle').removeClass('toggle-active');
+			});
+		}
+
+		//スマホ用ハンバーガーメニューが出た状態からリサイズされた場合もろもろ消す
+		if($(window).width() > 768){
+			$('#toggle-menu').css('display', 'none');
+			$('#toggle').removeClass('toggle-active');
+		}
+	})
+
 })
