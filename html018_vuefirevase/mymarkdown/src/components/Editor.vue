288329eac3420938827ac2c556ea1e593d5b5ea7
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-04-21 18:59:19 +0900

    追加 469まで

---
+++ html018_vuefirevase/mymarkdown/src/components/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="editor">
+		<h1>エディター画面</h1>
+	</div><!-- /.editor -->
+</template>
+
+<script>
+export default {
+	name:"editor",
+	data(){
+		return();
+	}
+};
+</script>
---
+++ html018_vuefirevase/mymarkdown/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="home">
+		<h1>{{msg}}</h1>
+		<button>googleアカウントでログイン</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "home",
+	data(){
+		return{
+			msg: "Welcome to MyMarkdown"
+		};
+	}
+};
+</script>
